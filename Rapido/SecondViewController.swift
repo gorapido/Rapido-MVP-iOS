02143524bc7bf67271d47ab7bc3537bfd9d98296
Alexander Hernandez <alex@alexanders-mbp.attlocal.net>
2015-04-10 19:13:07 -0400

    Initial Commit

---
+++ Rapido/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Rapido
+//
+//  Created by Alexander Hernandez on 4/10/15.
+//  Copyright (c) 2015 Rapido. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
